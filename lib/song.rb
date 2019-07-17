3b1329d0502f6c8bfc7bf621b7d3ec0db0eeebb2
Andrew Bay <daddnbay@yahoo.com>
2019-07-17 22:36:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
  
-  attr_accessor :artist
+  attr_accessor :title, :artist
  
-  def initialize(artist)
-    @artist = artist
+  def initialize(title)
+    @title = title
   end
  
 end
